142a83605023d79cb717e194e29629b6695a7164
Alexandr <alexandr.psk@gmail.com>
2018-09-22 01:08:28 +0300

    Add SelectionSort

---
+++ VisualSorting/Sortings/SelectionSort.cs
@@ -0,0 +1,25 @@
+﻿namespace VisualSorting.Sortings
+{
+    class SelectionSort : VisualSort
+    {
+        protected override void InternalSort(ISortingCollection collection)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                collection.Swap(i, FindMinIndex(collection, i));
+            }
+        }
+
+        private int FindMinIndex(ISortingCollection collection, int start)
+        {
+            var minIndex = start;
+
+            for (int i = start + 1; i < collection.Count; i++)
+            {
+                if (collection.Compare(i, minIndex) < 0)
+                    minIndex = i;
+            }
+            return minIndex;
+        }
+    }
+}
