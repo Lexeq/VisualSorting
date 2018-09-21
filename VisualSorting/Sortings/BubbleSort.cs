e1665110112dc9e678982fd5d502f2ef152637e1
Alexandr <alexandr.psk@gmail.com>
2018-09-22 00:48:30 +0300

    Add BubbleSort

---
+++ VisualSorting/Sortings/BubbleSort.cs
@@ -0,0 +1,22 @@
+﻿namespace VisualSorting.Sortings
+{
+    public class BubbleSort : VisualSort
+    {
+        protected override void InternalSort(ISortingCollection collection)
+        {
+            bool wasSwap;
+            do
+            {
+                wasSwap = false;
+                for (int i = 1; i < collection.Count; i++)
+                {
+                    if (collection.Compare(i, i - 1) < 0)
+                    {
+                        collection.Swap(i, i - 1);
+                        wasSwap = true;
+                    }
+                }
+            } while (wasSwap);
+        }
+    }
+}
