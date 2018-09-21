e93a989877b45444df6f7153858bc2cc9c2ef690
Alexandr <alexandr.psk@gmail.com>
2018-09-22 01:00:03 +0300

    Add InsertionSort

---
+++ VisualSorting/Sortings/InsertionSort.cs
@@ -0,0 +1,17 @@
+﻿namespace VisualSorting.Sortings
+{
+    class InsertionSort : VisualSort
+    {
+        protected override void InternalSort(ISortingCollection collection)
+        {
+            for (int i = 1; i < collection.Count; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    if (collection.Compare(j, j - i) < 0)
+                        collection.Swap(j, j - i);
+                }
+            }
+        }
+    }
+}
